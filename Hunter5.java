5a4a0e969d81981c1cc24500af6845ef90f03ae4
Mithun Ganesh E <mithunganesh1@gmail.com>
2017-06-21 10:37:09 +0530

    Create Hunter5.java

---
+++ Hunter5.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+class Hunter5{
+    public static void main(String arr[]){
+        Scanner scan=new Scanner(System.in);
+        System.out.println("ENTER THE NUMBER TO BE CONVERTED TO STRING:");
+        int n1=scan.nextInt();
+        int b[]=new int[(String.valueOf(n1)).length()];
+        int len=(String.valueOf(n1)).length();
+        int rem,j=0;
+        while((n1>0)&&(j<len)){
+            rem=n1%10;
+            b[j]=rem;
+            n1=n1/10;
+            j++;
+        }
+        Arrays.sort(b);
+        String s1="";
+        for(int i=0;i<len;i++){
+        s1=s1+Character.toString((char)(96+b[i]));
+        }
+        System.out.println("THE CONVERTED STRING IS:"+s1);
+    }
+}
