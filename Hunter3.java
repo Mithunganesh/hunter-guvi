f1f27c11db937f07539c7fc2adea35e464cc7e2a
Mithun Ganesh E <mithunganesh1@gmail.com>
2017-06-21 09:46:28 +0530

    Update Hunter3.java

--- Hunter3.java
+++ Hunter3.java
@@ -10,14 +10,24 @@ class Hunter3{
     for(int i=0;i<n;i++){
         a[i]=scan.nextInt();
     }
+    int count=0;
     Arrays.sort(a);
+    for(int i=0;i<n-1;i++)
+		{
+			if(a[i]==a[i+1])
+			{
+			count++;
+			}
+		}
+		if(count==0){
     System.out.println("ENTER THE INDEX VALUE");
     int k=scan.nextInt();
     if(k<n){
         System.out.println("THE VALUE IN THE INDEX "+k+" IS:"+a[k]);
     }else{
         System.out.println("ENTER THE PROPER INDEX VALUE...");
-    }
+    }}else{System.out.println("ARRAY VALUES ARE NOT UNIQUE..... ENTER UNIQUE VALUES...");}
     }
     
 }
+
